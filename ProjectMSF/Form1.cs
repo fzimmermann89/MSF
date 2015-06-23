3820018f8c6c34d059fa1d995bd82d15867fc603
halgurd <halgurdtaher@hotmail.com>
2015-06-23 11:06:57 +0200

    Console in Debugmodus hinzugefügt, System.Diagnostics eingebunden für Laufzeitmessung etc.

--- ProjectMSF/Form1.cs
+++ ProjectMSF/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace ProjectMSF
 {
     public partial class Form1 : Form
     {
+        [DllImport("kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
         public Form1()
         {
             InitializeComponent();
+#if DEBUG
+            AllocConsole();
+#endif
+            Console.WriteLine("Programm started.");
         }
     }
 }
