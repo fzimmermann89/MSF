eb68f7d9251a38de46a154ae69b5611f0110aa5f
halgurd <halgurdtaher@hotmail.com>
2015-06-25 13:32:58 +0200

    Numerik eingebaut

--- ClusterNum/ClusterNum/MainForm.Designer.cs
+++ ClusterNum/ClusterNum/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
             this.button1 = new System.Windows.Forms.Button();
@@ -36,6 +37,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.iterationTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // elementHost1
@@ -132,6 +134,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer iterationTimer;
 
        }
 }
--- ClusterNum/ClusterNum/NumIterator.cs
+++ ClusterNum/ClusterNum/NumIterator.cs
@@ -7,6 +7,46 @@ namespace ClusterNum
 {
     public class NumIterator
     {
+        int vertexCount;
+        int[][] adjMatrix;
 
+        double beta, sigma, delta;
+
+        List<double[]> xt = new List<double[]>();
+
+        public NumIterator(int[][] adjMatrix, double beta, double sigma, double delta)
+        {
+            this.beta = beta;
+            this.sigma = sigma;
+            this.delta = delta;
+            this.adjMatrix = adjMatrix;
+            this.vertexCount = adjMatrix.Length;
+            xt.Add(new double[vertexCount]);
+
+        }
+
+        public void iterate()
+        {
+            double[] oldxi=xt[xt.Count-1];
+            double[] newxi = new double[vertexCount];
+            for (int i = 0; i < vertexCount; i++)
+            {
+               
+                double sum = 0;
+                for (int j = 0; j < vertexCount; j++)
+                {
+                    sum += (double)adjMatrix[i][j] * intensity(oldxi[j]);
+                }
+                double tmp = beta * intensity(oldxi[i]) + sigma * sum + delta;
+                newxi[i] = tmp % (2.0 * Math.PI);
+            }
+
+            xt.Add(newxi);
+        }
+
+        public static double intensity(double x)
+        {
+            return (1 - Math.Cos(x)) / 2.0;
+        }
     }
 }
