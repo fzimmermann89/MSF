55ae0e71d14514411034e013a4367dd0dc4ede5d
Felix <fzimmermann89@gmail.com>
2015-07-09 20:46:43 +0200

    Tmat für schwerpunkt/relativ hinzugefügt

--- ClusterNum/ClusterNum/Helper.cs
+++ ClusterNum/ClusterNum/Helper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using Accord.Math;
 
 namespace ClusterNum
 {
@@ -24,7 +25,7 @@ namespace ClusterNum
                 if (strsplitarr.Length != nodeCount)
                 {
                     throw new FormatException("Matrix ist nicht quadratisch");
-                
+
                 }
                 double[] intsplitarr;
                 try
@@ -33,7 +34,7 @@ namespace ClusterNum
                 }
                 catch (FormatException ex)
                 {
-                   throw new FormatException("Matrix ungültig:\n" + ex.Message);
+                    throw new FormatException("Matrix ungültig:\n" + ex.Message);
                 }
 
                 for (int j = 0; j < nodeCount; j++)
@@ -64,11 +65,49 @@ namespace ClusterNum
                 }
 
                 string[] parts = match.Split(new string[] { " ", ":" }, StringSplitOptions.RemoveEmptyEntries);
-                
+
                 cluster[j] = Array.ConvertAll(parts, int.Parse);
                 Array.Sort(cluster[j]);
             }
             return cluster;
         }
+        public static double[,] tmat(int[][] cluster)
+            //berechnet transformation nach schwerpunkt/relativ-koordinaten
+        {
+            int nodecount = 0;
+            foreach (int[] curcluster in cluster) nodecount += curcluster.Length;
+            double[,] tmat = new double[nodecount, nodecount];
+
+            //oberer block-schwerpunkt->synchrone bewegung
+            for (int i = 0; i < cluster.Length; i++)
+            {
+                double value = 1.0 / (cluster[i].Length);
+                for (int j = 0; j < cluster[i].Length; j++)
+                {
+                    int node = cluster[i][j];
+                    tmat[i, node] = value;
+                }
+            }
+
+            //unterer block->abweichungen der knoten vom mittelwert
+            int row = cluster.Length;
+            for (int i = 0; i < cluster.Length; i++)
+            {
+                for (int j = 0; j < cluster[i].Length - 1; j++)
+                {
+                    for (int k = 0; k < cluster[i].Length; k++)
+                    {
+                        int negnode = cluster[i][k];
+                        tmat[row, negnode] = -1.0 / (cluster[i].Length - 1);
+                    }
+                    int posnode = cluster[i][j];
+                    tmat[row, posnode] = 1;
+                    row++;
+                }
+            }
+
+            return tmat;
+        }
+
     }
 }
