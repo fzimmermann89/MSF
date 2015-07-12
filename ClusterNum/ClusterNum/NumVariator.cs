df7218c18db2d9a26daca67e6aeb78ddf6f2f48c
Felix <fzimmermann89@gmail.com>
2015-07-12 15:47:30 +0200

    ljapunowpertubation einstellbar. ljapunowsum gleich durch rec teilen

--- ClusterNum/ClusterNum/NumVariator.cs
+++ ClusterNum/ClusterNum/NumVariator.cs
@@ -22,7 +22,7 @@ namespace ClusterNum
             }
         }
 
-        private const double ljapunowPertubation = 0.01;
+       // private const double ljapunowPertubation = 0.01;
         public double betamin, betamax, sigma, delta, noise, pertubation;
         public int betasteps, pre, rec;
 
@@ -194,23 +194,14 @@ b 0 0 0 0 0 0 a 0 0 0
                         if (etanodenum >= clusterTransform.Length) // unterer Block
                         {
                             Ljapunator punator = new Ljapunator(JMats, BMat, cluster, smts, beta, sigma, delta);
-
-                            punator.etat[0][etanodenum] = ljapunowPertubation;
+                            punator.etat[0][etanodenum] = pertubation;
                             punator.iterate(rec);
-                            if (!double.IsNaN(punator.ljapunowSum))
-                            {
-                                ljapunow[m] = Math.Max(punator.ljapunowSum, ljapunow[m]);
-                            }
-                        }
+                            ljapunow[m] = Math.Max(punator.ljapunowSum / (double)rec, ljapunow[m]);
+
                     }
                 }
-                for (int m = 0; m < clusterTransform.Length; m++)
-                {
-                    ljapunow[m] /= (double)rec;
                 }
 
-
-                //ausgabe
                 result ret_result = new result(beta, rms, ljapunow);
                 callback(ret_result);
 
